bf0ff245c43423536bd3f21181cfd4421e215d25
Wade Burch <braernoch@gmail.com>
2021-07-25 15:52:45 -0600

    Original commit

---
+++ MLI.hpp
@@ -0,0 +1,72 @@
+/*
+ * MLI.hpp
+ *
+ *  Created on: Jul 16, 2021
+ *      Author: braer
+ */
+
+#ifndef MLI_HPP_
+#define MLI_HPP_
+
+
+/* Includes */
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <eigen3/Eigen/Eigen>
+
+
+/* Namespaces */
+using namespace std;
+
+
+/* Typedef */
+typedef float Scalar;
+typedef Eigen::MatrixXf Matrix;
+typedef Eigen::VectorXf ColVector;
+
+
+/* Class */
+class MLI {
+
+public:
+	// ctor
+	MLI(vector<uint> top, float givenRate);
+
+	// train
+	void train(vector<ColVector*> input_data, vector<ColVector*> output_data);
+
+	// utility
+	void printLayers();
+	void printWeights();
+	void printDeltas();
+	void printCaches();
+
+private:
+	vector<uint> topology;
+
+	vector<ColVector*> neurons;
+	vector<ColVector*> caches;
+	vector<ColVector*> deltas;
+	vector<Matrix*> weights;
+
+	Scalar learnRate;
+	Scalar cost;
+	uint depth;
+
+	// propagators
+	void forwardProp(ColVector& input);
+	void backwardProp(ColVector& output);
+
+	// utility
+	void errCalc(ColVector& output);
+	void updateWeights();
+};
+
+
+#endif /* MLI_HPP_ */
